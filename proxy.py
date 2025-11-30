6828059a35bbf0cad1856273bd94864114614d34
Banino <ba.xu@campus.fct.unl.pt>
2025-11-30 15:56:26 +0000

    final??

--- proxy.py
+++ proxy.py
@@ -1,28 +1,37 @@
 #!/usr/bin/env python3
+
 import sys
+
 from threading import Thread
+
 import socket
+
 from queue import Queue
 
+import requests
 
-def requestTrackSeq(manifest, trackNum, sck, host):
-    numSeq = getSegmentsNum(manifest)
+def requestTrackSeq(manifest, trackNum, url, idx):
+
+    numSeq = getSegmentsNum(manifest) 
+
     startOffset = 2 + 5 * trackNum + numSeq * (trackNum-1)
 
-    print(startOffset, len(manifest))
+    track = manifest[startOffset - 5]
 
+    split = manifest[startOffset+idx].split(" ")
 
-    split = manifest[startOffset].split(" ")
     startIndex = int(split[0])
+
     size = int(split[1])
-    finalIndex = finalIndex = startIndex + size - 1 
 
-    moviePath = f"{movieName}/{movieName}-{trackNum}.mp4"
-    rangeHeader = f"Range: bytes={startIndex}-{finalIndex}\r\n"
-    (header, data) = getParcial(sck, host, moviePath, rangeHeader)
+    finalIndex = startIndex + size - 1
 
-    print(header.decode())
-    return
+    totalUrl = "http://" + url + "/" + movieName + "/" + track
+    headers = {
+    	"Range": f"bytes={startIndex}-{finalIndex}",
+	}
+    r = requests.get(totalUrl, headers=headers)
+    return r
 
 def getTracksNum(manifest):
     return int(manifest[1])
@@ -30,46 +39,12 @@ def getTracksNum(manifest):
 def getSegmentsNum(manifest):
     return int(manifest[6])
 
-def get(sck, host, path):
-    ## cria o GET https... HTTP/1.0
-    get = (f"GET /{path} HTTP/1.0\r\n"
-        f"Host: {host}\r\n"
-        f"\r\n")
-    sck.sendall(get.encode())
-    return processSckAnswer(sck)
-
-def getParcial(sck, host, path, header):
-    ## cria o GET https... HTTP/1.0
-    get = (f"GET /{path} HTTP/1.0\r\n"
-        f"Host: {host}\r\n"+
-        header
-        +f"\r\n")
-    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    s.connect((host,port))
-    s.sendall(get.encode())
-    return processSckAnswer(s)
-
-def processSckAnswer(sck):
-    header = "empty"
-    data = b""
-    while True:
-        chunk = sck.recv(4096)
-        if not chunk:
-            break
-        if header == "empty":
-            header = chunk
-        else:
-            data += chunk
-
-    return (header, data)
- 
 # producer task
-def producer(queue, sck, host, port):
-    sck.connect((host,port))
-    manifestPath = f"{movieName}/manifest.txt"
+def producer(queue, sck, url):
 
-    (_,manifest) = get(sck, host, manifestPath)
-    manifest = manifest.decode().split("\n")
+    totalUrl = "http://" + url + "/" + movieName + "/" + "manifest.txt"
+    r = requests.get(totalUrl)
+    manifest = r.text.split("\n")
 
     ## se a track não existir
     if(getTracksNum(manifest) < trackNum):
@@ -77,43 +52,43 @@ def producer(queue, sck, host, port):
         return
     
     numSeg = getSegmentsNum(manifest)
-
-    requestTrackSeq(manifest, trackNum, sck, host)
+    for i in range(numSeg):
+        r = requestTrackSeq(manifest, trackNum, url, i)
+        queue.put(r.content)
 
     queue.put(None)
 
     print('Producer: Done')
- 
+
 # consumer task
-def consumer(queue):
+def consumer(queue, sck):
     while True:
         item = queue.get()
         if item is None:
             break
+        sck.sendall(item)
     print('Consumer: Done')
 
 url = sys.argv[1]
+
 movieName = sys.argv[2]
+
 trackNum = int(sys.argv[3])
 
 splitHostPort = url.split(":")
-host = splitHostPort[0]
-port = int(splitHostPort[1])
 
 sck = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
+sck.connect(("",8000))
+
 queue = Queue()
 
-consumer = Thread(target=consumer, args=(queue,))
+consumer = Thread(target=consumer, args=(queue, sck))
+
 consumer.start()
 
-producer = Thread(target=producer, args=(queue,sck, host, port))
+producer = Thread(target=producer, args=(queue,sck, url))
 producer.start()
 
 producer.join()
 consumer.join()
-
-
-
-    
-
